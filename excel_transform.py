67391ccf7a05355858183525eda14367e33b155d
juliuspumpkin <134052310+juliuspumpkin@users.noreply.github.com>
2023-05-19 13:55:34 +0300

    Update excel_transform.py

--- excel_transform.py
+++ excel_transform.py
@@ -3,19 +3,34 @@ import pandas as pd
 # Read the Excel file into a DataFrame
 df = pd.read_excel('input.xlsx')
 
-# Create a new DataFrame with the desired columns and data
-new_df = pd.DataFrame({
-    'ROW NO': range(1, len(df) + 1),
-    'Names': 'OMNIUBUS',
-    'Code': '7831505',
-    'Debit Amount': df['AMOUNT'],
-    'Credit Amount': '',
-    'Pages': range(1, len(df) + 1)
-})
+# Create a new DataFrame to store the transformed data
+new_df = pd.DataFrame(columns=['ROW NO', 'Names', 'Code', 'Debit Amount', 'Credit Amount', 'Pages'])
 
-# Add additional columns to new_df
-new_df['Names'] += ' ' + df['Client Name'] + ' ON BEHALF OF BESTBANK'
-new_df['Credit Amount'] = df['AMOUNT']
+# Iterate over each row in the source DataFrame
+for index, row in df.iterrows():
+    # Empty row
+    new_df = new_df.append(pd.Series(dtype='object'), ignore_index=True)
 
-# Save the new DataFrame to a new Excel file
+    # Second row
+    new_df = new_df.append({
+        'Names': 'OMNIUBUS',
+        'Code': '7831505',
+        'Debit Amount': row['AMOUNT']
+    }, ignore_index=True)
+
+    # Third row
+    new_df = new_df.append({
+        'ROW NO': index + 1,
+        'Pages': index + 1,
+        'Names': 'SMP',
+        'Code': '22001100300000000RMB002Q',
+        'Credit Amount': row['AMOUNT']
+    }, ignore_index=True)
+
+    # Fourth row
+    new_df = new_df.append({
+        'Names': row['Client Name']
+    }, ignore_index=True)
+
+# Save the transformed DataFrame to a new Excel file
 new_df.to_excel('output.xlsx', index=False)
