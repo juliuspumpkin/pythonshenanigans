31a851a6ecb4dd5b5e629417e2341b88d0e23955
juliuspumpkin <134052310+juliuspumpkin@users.noreply.github.com>
2023-05-19 14:46:35 +0300

    Update borders.py

--- borders.py
+++ borders.py
@@ -8,16 +8,47 @@ workbook = openpyxl.load_workbook('output.xlsx')
 worksheet = workbook.active
 
 # Define border styles
-border = Border(left=Side(border_style='thin'),
-                right=Side(border_style='thin'),
-                top=Side(border_style='thin'),
-                bottom=Side(border_style='thin'))
+thin_border = Border(left=Side(border_style='thin'),
+                     right=Side(border_style='thin'),
+                     top=Side(border_style='thin'),
+                     bottom=Side(border_style='thin'))
+left_right_border = Border(left=Side(border_style='thin'),
+                           right=Side(border_style='thin'))
+left_bottom_border = Border(left=Side(border_style='thin'),
+                            right=Side(border_style='thin'),
+                            bottom=Side(border_style='thin'))
+right_bottom_border = Border(right=Side(border_style='thin'),
+                             bottom=Side(border_style='thin'))
 
-# Apply border to columns A to F
-for column in ['A', 'B', 'C', 'D', 'E', 'F']:
-    for row in range(1, worksheet.max_row + 1):
-        cell = worksheet[column + str(row)]
-        cell.border = border
+# Apply borders to the header row
+header_row = worksheet[1]
+for cell in header_row:
+    cell.border = thin_border
+
+# Apply borders to each group of 4 rows
+for row_index in range(2, worksheet.max_row + 1, 4):
+    # First row in the group
+    first_row = worksheet[row_index]
+    for cell in first_row:
+        cell.border = thin_border
+
+    # Second row in the group
+    second_row = worksheet[row_index + 1]
+    for cell in second_row:
+        if cell.column_letter == 'D':
+            cell.border = thin_border
+        else:
+            cell.border = left_right_border
+
+    # Third row in the group
+    third_row = worksheet[row_index + 2]
+    for cell in third_row:
+        cell.border = left_right_border
+
+    # Fourth row in the group
+    fourth_row = worksheet[row_index + 3]
+    for cell in fourth_row:
+        cell.border = right_bottom_border
 
 # Save the modified workbook
 workbook.save('output.xlsx')
